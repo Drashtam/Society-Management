22ddce70dd333b1ed19d2eaf22284574548badd6
Drashtam Banker <55189624+Drashtam@users.noreply.github.com>
2022-10-14 22:41:52 +0530

    Update MainActivity2.java

--- app/src/main/java/com/example/project/MainActivity2.java
+++ app/src/main/java/com/example/project/MainActivity2.java
@@ -27,13 +27,6 @@ public class MainActivity2 extends AppCompatActivity {
                 finish();
             }
         },3000);
-        /*FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        
     }
 }
